6ae73b4a110f015eadd6371c600d75dcc6122c79
Yonatan-Ethiopia <83534269+Yonatan-Ethiopia@users.noreply.github.com>
2025-05-09 12:50:48 +0300

    Update main.py
    Added binary cross entropy for to calculate the loss

--- main.py
+++ main.py
@@ -12,3 +12,8 @@ def predict(x):
     z = np.dot(x, weights)+bias
     prediction = sigmoid(z)
     return prediction 
+def binary_cross_entropy(y_true, y_pred):
+    epsilon = 1e-15
+    y_pred = np.clip( y_pred, epsilon, 1- epsilon )
+    loss = y_true*-1*np.log(y_pred)+(1-y_pred)*-1*np.log(1-y_pred)
+    return np.mean(loss)

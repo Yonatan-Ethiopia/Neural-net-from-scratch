fddb06b7269cc4123b2563164d6dfcae9756480a
Yonatan-Ethiopia <83534269+Yonatan-Ethiopia@users.noreply.github.com>
2025-05-13 21:58:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ inputs = np.array([[0,1], [1,0], [1,1], [0,0]])
 lnr_initial = 0.1
 decay_rate = 0.001
 target =np.array([ 1, 1, 0, 0]).reshape(-1,1)
-epoch = 1000
+epoch = 500
 weights = np.random.randn(2,1)
 bias = 1
 def sigmoid(x):
@@ -19,16 +19,20 @@ def binary_cross_entropy(y_true, y_pred):
     loss = y_true*-1*np.log(y_pred)+(1-y_true)*-1*np.log(1-y_pred)
 p, z = predict(inputs)
 def backprop(x, p, t):
-    dLdW = ( -t/p +(1-t)/(1-p))*(p*(1-p))*x
-    dLDb = ( -t/p +(1-t)/(1-p))*(p*(1-p))
-    return dLdW, dLDb
+    m = len(x)
+    dZ = p - t
+    dW = (1/m) * np.dot(x.T, dZ)
+    dB = (1/m) * np.sum(dZ)
+    return dW,dB
 
 for i in range(1, 1+epoch):
-    lnr = lnr_initial/(1 + decay_rate*epoch)
+    lnr = lnr_initial/(1 + decay_rate*i)
     p, z = predict(inputs)
     loss = binary_cross_entropy( target, p)
     dw,db = backprop(inputs, p, target)
     weights -= lnr*dw
     bias -= lnr*db
-    if( epoch%100 ):
-        print(weights, lnr, bias)
+    if( epoch%10 == 0):
+        print("weight : ",weights)
+        print("bias : ",bias)
+        print("learningrate : ",lnr)
